5b58078573ab8ac2f73ad71de82a98bd23d5ceef
oscark6 <oscar.kimani1@student.moringaschool.com>
2024-07-23 12:12:22 +0300

    done

--- server/app.py
+++ server/app.py
@@ -20,8 +20,36 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
 
+
+# Add views here
+@app.route('/earthquakes/<int:id>')
+def Earthquake_by_id(id):
+   earthquake =Earthquake.query.filter(Earthquake.id == id).first()
+
+   if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+   else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+   return make_response(body, status)
+
+
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+    quakes = []  # array to store a dictionary for each earthquake
+    for quake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        quakes.append(quake.to_dict())
+
+    body = {'count': len(quakes),
+            'quakes': quakes
+            }
+    
+    return make_response(body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
